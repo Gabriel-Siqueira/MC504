1efbc0de76b3548fafc11cb1d6b1e4d2f015ec66
Racoci <LRacoci@users.noreply.github.com>
2016-06-30 23:35:10 -0300

    Starting coments on Bit Map

--- BitMap.py
+++ BitMap.py
@@ -1,43 +1,73 @@
+"""
+    Defines structure of bit map to handle all the blocks of memory
+"""
+
+
 class BitMap:
+    """
+        Constructor:
+            size: Is the total number of memory blocks
+    """
     def __init__(self, size):
         self.size = size
-        self.bitMap = [0]*size
+        # Starts all bits with zero
+        self.bitMap = [0] * size
+        # Last address for the next fit
         self.add = 0
-    
+        # Virtual time counter of each operation
+        self.time = 0
+
     def __str__(self):
         s = ""
         for bit in self.bitMap:
-            s+=str(bit)
+            s += str(bit)
 
         return s + "\n"
-    
+
     def __repr__(self):
         return str(self)
-        
+    """
+        It tries to allocate in the first set of congruent free blocks that fits the requisition
+            alloc_size: Requested size to allocate
+    """
     def first_fit(self, alloc_size):
+        # Starts to count the virtual time
         self.time = 0
+        # Initialization of the address that will iterate through all the bits in the Bit Map
         add = 0
+        # Condition of iteration
         while add < self.size:
+            # For each access in the memory that will be made in the comparison of the if right below
             self.time += 1
+            # If the first block is free
             if self.bitMap[add] == 0:
+                # Tries to find a set of alloc_size congruent free blocks
                 i = 0
+                # While only less then alloc_size valid free blocks are found
                 while i < alloc_size and add + i < self.size and self.bitMap[add + i] == 0:
+                    # Counts an access in the structure
                     self.time += 1
+                    # Increment the size already found
                     i += 1
+                # If a set of alloc_size blocks is found
                 if i == alloc_size:
+                    # Allocate that block
                     i = 0
                     while i < alloc_size:
                         self.time += 1
                         self.bitMap[add + i] = 1
                         i += 1
                     return add
+                # Else, jumps that set of blocks
                 else:
                     add += i
+            # Else tries the next block
             else:
                 add += 1
+        # If address found is out of range returns error
         if add >= self.size:
             return -1
-        
+
     def next_fit(self, alloc_size):
         self.time = 0
         add_begin = self.add
